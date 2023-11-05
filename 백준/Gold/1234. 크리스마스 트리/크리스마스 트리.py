bbd367f41e324f1cedc3fc947d6e23cb05268ea2
kim <83156897+kyoungbinkim@users.noreply.github.com>
2023-11-05 17:55:04 +0900

    [Gold II] Title: 크리스마스 트리, Time: 1004 ms, Memory: 32952 KB -BaekjoonHub

---
+++ 백준/Gold/1234. 크리스마스 트리/크리스마스 트리.py
@@ -0,0 +1,36 @@
+import math
+import sys
+sys.setrecursionlimit(10**9)
+
+def calc(rbg, n):
+    ret = 0
+    r,b,g = rbg
+    if min(rbg) < 0:
+        return 0
+
+    if n==1:
+        for i in rbg:
+            if i>0:
+                ret +=1
+        return ret
+
+    if n%2 == 0:
+        halfn = n//2
+        c = math.comb(n, halfn)
+        ret += c * (calc([r-halfn, b-halfn, g], n-1) \
+            +  calc([r-halfn, b, g-halfn], n-1)  \
+                + calc([r, b-halfn, g-halfn], n-1))
+    
+    if n%3 == 0:
+        treen = n//3
+        c = math.comb(n, treen) * math.comb(n-treen, treen)
+        ret += c * calc([r-treen, b-treen, g-treen], n-1)
+    
+    ret += (calc([r-n,b,g], n-1) + calc([r,b-n,g], n-1) + calc([r,b,g-n], n-1))
+
+    return ret
+
+
+N,R,B,G = map(int, input().split())
+print(calc([R,B,G],N))
+
