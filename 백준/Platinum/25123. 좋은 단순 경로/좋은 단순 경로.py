f62688caa716ffaaa99942c57d7771dec1d6cf14
kim <83156897+kyoungbinkim@users.noreply.github.com>
2024-06-15 16:10:04 +0900

    [Platinum V] Title: 좋은 단순 경로, Time: 3052 ms, Memory: 241796 KB, Score: 30 point -BaekjoonHub

--- 백준/Platinum/25123. 좋은 단순 경로/좋은 단순 경로.py
+++ 백준/Platinum/25123. 좋은 단순 경로/좋은 단순 경로.py
@@ -1,5 +1,6 @@
 from sys import stdin
 from heapq import heappush, heappop
+from collections import deque
 
 def solve():
     maxlen = 1
@@ -14,47 +15,75 @@ def solve():
         if c < 0:
             continue
         degs[c] += 1
+    
+    que = deque([])
+    for i in range(n):
+        if degs[i] == 0 and p[i] >= 0:
+            que.append(i)
     # print("p : ", p)
     # print("degs : ", degs)
     
     vis = set()
-    for _ in range(n):
-        for i in range(n):
-            if p[i] >= 0 and degs[i] == 0 and i not in vis:
-                # print(i)
-                vis.add(i)
-                degs[p[i]] -= 1
+    while len(que):
+        i = que.popleft()
+        # print(i)
+        # print(ans, maxlen, maxcnt, vis)
+        
+            
+        # print(i)
+        vis.add(i)
+        degs[p[i]] -= 1
+        if degs[p[i]] == 0 and not p[i] in vis and p[p[i]] >= 0:
+            que.append(p[i])
 
-                if maxlen < ans[i][0]:
-                    maxlen = ans[i][0]
-                    maxcnt = ans[i][1]
-                elif maxlen == ans[i][0]:
-                    maxcnt += ans[i][1]
+        if maxlen < ans[i][0]:
+            maxlen = ans[i][0]
+            maxcnt = ans[i][1]
+        elif maxlen == ans[i][0]:
+            maxcnt += ans[i][1]
                 
 
-                for tmp1 in range(len(ans[i][2])):
-                    for tmp2 in range(tmp1+1, len(ans[i][2])):
-                        idx1, idx2 = ans[i][2][tmp1], ans[i][2][tmp2]
-                        if ans[idx1][0] + ans[idx2][0]+ 1> maxlen:
-                            maxlen = ans[idx1][0] + ans[idx2][0]+ 1
-                            maxcnt = ans[idx1][1] * ans[idx2][1]
-                        elif ans[idx1][0] + ans[idx2][0]+ 1 == maxlen:
-                            maxcnt += ans[idx1][1] * ans[idx2][1]
                 
-                if alpha[i] == alpha[p[i]]:
-                    break
+        if len(ans[i][2]) > 1:
+            tmp1 = heappop(ans[i][2])
+            if tmp1[0] == ans[i][2][0][0]:
+                if -2 * tmp1[0] +  1 >= maxlen:
+                            maxcnt = maxcnt if maxlen == -2 * tmp1[0] +  1 else 0
+                            maxlen = -2 * tmp1[0] +  1
+                            tmp = [tmp1]
+
+                            # print(ans[i][2], tmp1, tmp, "??")
+                            while len(ans[i][2]) and tmp1[0] == ans[i][2][0][0]:
+                                ttt = heappop(ans[i][2])
+                                for can in tmp:
+                                    maxcnt += can[1] * ttt[1]
+                                tmp.append(ttt)
+            else:
+                        tmp = heappop(ans[i][2])
+
+                        if -tmp[0]-tmp1[0] + 1 >= maxlen:
+                            maxcnt = maxcnt+ tmp[1]*tmp1[1] if maxlen == -tmp[0]-tmp1[0] + 1 else tmp[1]*tmp1[1]
+                            maxlen = -tmp[0]-tmp1[0] + 1
+                            
+                            while len(ans[i][2]) and tmp[0] == ans[i][2][0][0]:
+                                ttt = heappop(ans[i][2])
+                                maxcnt += tmp1[1] * ttt[1]
+                    
+                
+        if alpha[i] == alpha[p[i]]:
+            continue
 
                 
-                ans[p[i]][2].append(i)
+        heappush(ans[p[i]][2], (-ans[i][0], ans[i][1]))
 
-                if ans[p[i]][0] < ans[i][0]+1:
-                    ans[p[i]][0] = ans[i][0]+1
-                    ans[p[i]][1] = ans[i][1]
-                elif ans[p[i]][0] == ans[i][0]+1:
-                    ans[p[i]][1] += ans[i][1]
-                break
-    #     print(ans, maxlen, maxcnt, vis)
-    # print(ans, maxlen, maxcnt, vis)
+        if ans[p[i]][0] < ans[i][0]+1:
+            ans[p[i]][0] = ans[i][0]+1
+            ans[p[i]][1] = ans[i][1]
+        elif ans[p[i]][0] == ans[i][0]+1:
+            ans[p[i]][1] += ans[i][1]
+
+    
+    # print('~~', ans, maxlen, maxcnt, vis)
 
     for i in range(n):
         if p[i] < 0:
@@ -65,20 +94,34 @@ def solve():
                 maxcnt += ans[i][1]
                 
             # print("!!", ans, maxlen, maxcnt, vis)
-            for tmp1 in range(len(ans[i][2])-1):
-                for tmp2 in range(tmp1+1, len(ans[i][2])):
-                    idx1, idx2 = ans[i][2][tmp1], ans[i][2][tmp2]
-                    if ans[idx1][0] + ans[idx2][0]+ 1> maxlen:
-                        maxlen = ans[idx1][0] + ans[idx2][0]+ 1
-                        maxcnt = ans[idx1][1] * ans[idx2][1]
-                    elif ans[idx1][0] + ans[idx2][0]+ 1 == maxlen:
-                        maxcnt += ans[idx1][1] * ans[idx2][1]
+            if len(ans[i][2]) > 1:
+                    tmp1 = heappop(ans[i][2])
+                    # print(tmp1, ans[i][2])
+                    if tmp1[0] == ans[i][2][0][0]:
+                        if -2 * tmp1[0] +  1 >= maxlen:
+                            maxcnt = maxcnt if maxlen == -2 * tmp1[0] +  1 else 0
+                            maxlen = -2 * tmp1[0] +  1
+                            tmp = [tmp1]
+
+                            # print(ans[i][2], tmp1, tmp, "??")
+                            while len(ans[i][2]) and tmp1[0] == ans[i][2][0][0]:
+                                ttt = heappop(ans[i][2])
+                                for can in tmp:
+                                    maxcnt += can[1] * ttt[1]
+                                tmp.append(ttt)
+                    else:
+                        tmp = heappop(ans[i][2])
+                        if -tmp[0]-tmp1[0] + 1 >= maxlen:
+                            maxcnt = maxcnt + tmp[1]*tmp1[1] if maxlen == -tmp[0]-tmp1[0] + 1 else tmp[1]*tmp1[1]
+                            maxlen = -tmp[0]-tmp1[0] + 1
+                            
+                            while len(ans[i][2]) and tmp[0] == ans[i][2][0][0]:
+                                ttt = heappop(ans[i][2])
+                                maxcnt += tmp1[1] * ttt[1]
             break
     # print(ans, maxlen, maxcnt, vis)
+    # print("!!!::", maxlen, maxcnt)
     print(maxlen, maxcnt)
-    
-
-
-            
+          
 for _ in range(int(stdin.readline())):
     solve()
